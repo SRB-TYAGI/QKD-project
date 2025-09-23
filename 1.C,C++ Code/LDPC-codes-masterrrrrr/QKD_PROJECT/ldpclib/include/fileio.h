4bbb077ddccab8606c3066b57baf1c2e22924f27
Sourabh Tyagi <131552707+SRB-TYAGI@users.noreply.github.com>
2025-09-23 23:55:09 +0530

    Delete 1.C,C++ Code/LDPC-codes-masterrrrrr directory

--- 1.C,C++ Code/LDPC-codes-masterrrrrr/QKD_PROJECT/ldpclib/include/fileio.h
+++
@@ -1,9 +0,0 @@
-// fileio.h
-#ifndef FILEIO_H
-#define FILEIO_H
-
-#include <cstdio>
-// FILE* open_file_std(char* name, char* mode);
-FILE*open_file_std(const char* name, const char* mode);
-
-#endif
--- 1.C,C++ Code/LDPC-codes-masterrrrrr/QKD_PROJECT/ldpclib/source/fileio.cpp
+++
@@ -1,5 +0,0 @@
-// fileio.cpp
-#include <cstdio>
-FILE* open_file_std(const char* name, const char* mode) {
-return fopen(name, mode);
-}
